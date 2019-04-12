fb609d1157908f7306b1435408ae71eac197a581
151150074 <151150074@smail.nju.edu.cn>
2019-04-12 10:22:32 +0800

    修

--- pai_algorithm/pre/data_split.py
+++ pai_algorithm/pre/data_split.py
@@ -10,9 +10,8 @@ def split(request):
     X_train = json_result['X_train']
     Y_train = json_result['y_train']
     test_per=json_result['test_size']
-    random_num= json_result['random_state']
     X_train, X_test, y_train, y_test = train_test_split(X_train, Y_train, test_size=test_per,
-                                                                         random_state=random_num)
+                                                                         random_state=random.randint(0, 100))
     result = {"X_train":X_train,
               "y_train":y_train,
               "X_test":X_test,
