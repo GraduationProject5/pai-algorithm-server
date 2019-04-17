09716e2bd60e14a79fdb1a04e7a5d3b78eee8bb1
chenpipin <gmdchanbb@gmail.com>
2019-04-17 16:19:31 +0800

    LookupError: No installed app with label 'admin'.

--- pai_algorithm/settings.py
+++ pai_algorithm/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
+
+AUTH_USER_MODEL = "user.UserProfile"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
