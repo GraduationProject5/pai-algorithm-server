19c57110a4e828a2abfe0a52fa9b5d93a4797bd8
chenpipin <gmdchanbb@gmail.com>
2019-04-26 17:04:40 +0800

    modify the setting.py

--- pipenv_run_celery.sh
+++ pipenv_run_celery.sh
@@ -1,3 +1,2 @@
 #/bin/sh
-pipenv run python manage.py runserver 0.0.0.0:14000
 pipenv run celery -A pai_algorithm worker -l info
---
+++ pipenv_run_sever.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+pipenv run python manage.py runserver 0.0.0.0:14000
